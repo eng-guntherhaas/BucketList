cf864f75c6e0e2c4249560da286ee2b5c9b4c7b1
guntherhaas <guntherhaas.eng@gmail.com>
2024-07-24 16:09:19 +0200

    initial commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Gunther Masi Haas on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
